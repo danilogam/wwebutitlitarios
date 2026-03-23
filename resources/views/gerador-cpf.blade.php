4376f3adcd46290e4827179f54a87dd5ef12c81d
danilogam <danilo.s.gameiro@gmail.com>
2026-03-23 15:18:47 -0300

    Style Gerador de CPF page to match bcrypt-hash

--- resources/views/gerador-cpf.blade.php
+++ resources/views/gerador-cpf.blade.php
@@ -8,81 +8,73 @@
 
 @section('content')
 
-<div class="container mt-4">
-
-  <div class="row">
-    <div class="col-md-12" style="text-align: center">
-
-      <div class="card">
-        <div class="card-body">
-
-          <h3 class="mb-4">Gerador de CPF</h3>
-
-          <div class="row">
-
-            <div class="col-md-6">
-
-              <label>Gerar com pontuação?</label>
-
-              <div>
-                <input type="radio" name="pontuacao" value="sim" checked> Sim
-                <input type="radio" name="pontuacao" value="nao"> Não
-              </div>
-
-            </div>
-
-            <div class="col-md-6">
-
-              <label>Estado de Origem</label>
-
-              <select class="form-control" id="estado">
-
-                <option value="indiferente">Indiferente</option>
-                <option value="0">RS</option>
-                <option value="1">DF, GO, MS, MT, TO</option>
-                <option value="2">AC, AM, AP, PA, RO, RR</option>
-                <option value="3">CE, MA, PI</option>
-                <option value="4">AL, PB, PE, RN</option>
-                <option value="5">BA, SE</option>
-                <option value="6">MG</option>
-                <option value="7">ES, RJ</option>
-                <option value="8">SP</option>
-                <option value="9">PR, SC</option>
-
-              </select>
-
-            </div>
-
-          </div>
-
-          <button class="btn btn-primary mt-3" id="gerarCpf">
-            Gerar CPF
-          </button>
-
-          <div class="mt-4">
-
-            <label>CPF Gerado</label>
-
-            <div class="input-group">
-
-              <input type="text" id="cpfGerado" class="form-control" readonly>
-
-              <button class="btn btn-outline-secondary" type="button" id="copiarCpf" title="Copiar">
-                <i class="bi bi-clipboard"></i>
-              </button>
-
-            </div>
-
-          </div>
-
+<div class="py-5">
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="text-center mb-4">Gerador de CPF</h1>
         </div>
-      </div>
-
     </div>
 
-  </div>
-  
+    <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+            <div class="card shadow-sm">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-6 mb-3">
+                            <label class="form-label">Gerar com pontuação?</label>
+                            <div class="d-flex gap-3">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="pontuacao" id="pontSim" value="sim" checked>
+                                    <label class="form-check-label" for="pontSim">Sim</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="pontuacao" id="pontNao" value="nao">
+                                    <label class="form-check-label" for="pontNao">Não</label>
+                                </div>
+                            </div>
+                        </div>
 
+                        <div class="col-md-6 mb-3">
+                            <label class="form-label">Estado de Origem</label>
+                            <select class="form-select" id="estado">
+                                <option value="indiferente">Indiferente</option>
+                                <option value="0">RS</option>
+                                <option value="1">DF, GO, MS, MT, TO</option>
+                                <option value="2">AC, AM, AP, PA, RO, RR</option>
+                                <option value="3">CE, MA, PI</option>
+                                <option value="4">AL, PB, PE, RN</option>
+                                <option value="5">BA, SE</option>
+                                <option value="6">MG</option>
+                                <option value="7">ES, RJ</option>
+                                <option value="8">SP</option>
+                                <option value="9">PR, SC</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="d-grid gap-2">
+                        <button class="btn btn-primary btn-lg" id="gerarCpf">
+                            GERAR CPF
+                        </button>
+                    </div>
+
+                    <hr class="my-4">
+
+                    <div class="mb-3">
+                        <label class="form-label">CPF Gerado</label>
+                        <div class="input-group">
+                            <input type="text" id="cpfGerado" class="form-control form-control-lg" readonly>
+                            <button class="btn btn-secondary" type="button" id="copiarCpf">
+                                COPIAR
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3"></div>
+    </div>
 </div>
 
 <script>
